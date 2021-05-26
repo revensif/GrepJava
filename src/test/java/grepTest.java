c6d3c56b78ce41082b8758a78e6962f06c7ca48b
Revensif <revensif@mail.ru>
2021-05-26 16:23:41 +0300

    Test

--- src/test/java/grepTest.java
+++ src/test/java/grepTest.java
@@ -1,12 +1,50 @@
 import junit.framework.TestCase;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
 
 public class grepTest extends TestCase {
 
-    public void testMain() {
+    public void testCheck() {
+        String result = "Нет";
+        String line = "Как сегодня хорошо";
+        String word = "как";
+        String[] words = line.split(" ");
+        for (String i : words) {
+            if (i.equals(word)) {
+                result = "Есть";
+                break;
+            }
+        }
+        assertEquals(result, "Нет");
+    }
+
+    public void testCheckR() {
+        String result = "Нет";
+        String line = "Как сегодня хорошо";
+        String word = "год";
+         if (line.matches("(.*)" + word + "(.*)")) result = "Есть";
+        assertEquals(result, "Есть");
+    }
+
+    public void testCheckI() {
+        String result = "Нет";
+        String line = "Как сегодня хорошо";
+        String word = "как";
+        String[] words = line.split(" ");
+        for (String i : words) {
+            if (i.equalsIgnoreCase(word)) {
+                result = "Есть";
+                break;
+            }
+        }
+        assertEquals(result, "Есть");
+    }
+
+    public void testCheckIR() {
+        String result = "Нет";
+        String line = "Как сегодня хорошо";
+        String word = "ГОД";
+        if (line.toLowerCase().contains(word.toLowerCase())) result = "Есть";
+        assertEquals(result, "Есть");
     }
 }
+
